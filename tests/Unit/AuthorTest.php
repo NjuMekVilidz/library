d2744f0c8ee36b971fda19b34361a61a878ac549
Nikola Vulovic <vule93ca@gmail.com>
2020-12-26 13:02:10 +0100

    Deleted method for author test

--- tests/Unit/AuthorTest.php
+++ tests/Unit/AuthorTest.php
@@ -10,13 +10,4 @@ class AuthorTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
-    public function only_name_is_required_to_create_an_author()
-    {
-        Author::query()->firstOrCreate([
-           'name' => 'John Doe'
-        ]);
-
-        $this->assertCount(1, Author::all());
-    }
 }
